c5e0a070581eef81573408345d58f7612c4ab291
mountainguy76935 <stoll33david@gmail.com>
2020-04-24 19:48:03 -0600

    server

---
+++ src/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+
+app.use(express.static(path.join(__dirname, 'build')));
+  
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname,  "build", "index.html"));
+  });
+
+app.listen(process.env.PORT || 8080);
